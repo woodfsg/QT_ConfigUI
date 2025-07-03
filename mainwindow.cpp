3681b746bf64cf28133547b748e42a37526b5eeb
ZhangHui <zhanghui@szxxbio.com>
2025-07-03 14:58:48 +0800

    [add] QTreeWidget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,40 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // 初始化下拉框映射
+    m_dataMap["请选择程序类别"] = QStringList() << "请选择子类别";
+    m_dataMap["信息输入"] = QStringList() << "苹果" << "香蕉" << "橙子" << "葡萄";
+    m_dataMap["信息提示"] = QStringList() << "胡萝卜" << "西兰花" << "菠菜" << "土豆";
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_category_currentTextChanged(const QString &text)
+{
+    ui->subCategory->clear();
+    QStringList subCategory = m_dataMap.value(text);
+    //change subCategory items
+    ui->subCategory->addItems(subCategory);
+    //change parameter configuration forms
+    if (text == "请选择程序类别") {
+        ui->paramConfig->setCurrentIndex(2);
+    } else if (text == "信息输入") {
+        ui->paramConfig->setCurrentIndex(0);
+    } else if (text == "信息提示") {
+        ui->paramConfig->setCurrentIndex(1);
+    }
+}
+
+void MainWindow::on_addStage_clicked()
+{
+    return;
+}
+
+void MainWindow::on_deleteItem_clicked()
+{
+    return;
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMap>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,12 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QMap<QString, QStringList> m_dataMap;
+
+private slots:
+    void on_category_currentTextChanged(const QString &text);
+    void on_addStage_clicked();
+    void on_deleteItem_clicked();
+
 };
 #endif // MAINWINDOW_H
